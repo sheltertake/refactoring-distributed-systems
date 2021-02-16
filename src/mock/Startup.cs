5c4241503a3dd9fdadd23f8841018a664a4d9889
Massimiliano Balestrieri <massimiliano.balestrieri@deltatre.it>
2021-02-16 11:04:24 +0100

    mock - cuncurrent

--- src/mock/Startup.cs
+++ src/mock/Startup.cs
@@ -1,13 +1,14 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace mock
@@ -16,14 +17,7 @@ namespace mock
     public class Startup
     {
         private static int COUNTER;
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton(sp => new MockContext(new DbContextOptionsBuilder<MockContext>().UseInMemoryDatabase(databaseName: "mock").Options));
-        }
-
+        private static ConcurrentBag<Order> Orders = new ConcurrentBag<Order>();
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
@@ -48,52 +42,53 @@ namespace mock
                         }
                     }
 
-                    var body = await JsonSerializer.DeserializeAsync<OrderRequest>(context.Request.Body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                    var dbContext = context.RequestServices.GetRequiredService<MockContext>();
-                    await dbContext.Orders.AddAsync(body);
-                    await dbContext.SaveChangesAsync();
+                    var body = await JsonSerializer.DeserializeAsync<Order>(context.Request.Body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    //var dbContext = context.RequestServices.GetRequiredService<MockContext>();
+                    //await dbContext.Orders.AddAsync(body);
+                    //await dbContext.SaveChangesAsync();
+                    Orders.Add(body);
                     context.Response.StatusCode = 200;
                 });
                 endpoints.MapGet("/", async context =>
                 {
-                    var dbContext = context.RequestServices.GetRequiredService<MockContext>();
-                    var items = await dbContext.Orders.ToListAsync();
+                    //var dbContext = context.RequestServices.GetRequiredService<MockContext>();
+                    //var items = await dbContext.Orders.ToListAsync();
                     await context.Response.WriteAsJsonAsync(new MockReportResponse
                     {
                         Counter = COUNTER,
-                        Items = items,
-                        Errors = COUNTER - items.Count
+                        Items = Orders.ToArray(),
+                        Errors = COUNTER - Orders.Count
                     });
                 });
             });
         }
     }
 
-    public class MockContext : DbContext
-    {
-        public MockContext(DbContextOptions options) : base(options)
-        {
-        }
-        public virtual DbSet<OrderRequest> Orders { get; set; }
+    //public class MockContext : DbContext
+    //{
+    //    public MockContext(DbContextOptions options) : base(options)
+    //    {
+    //    }
+    //    public virtual DbSet<OrderRequest> Orders { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<OrderRequest>(entity =>
-            {
-                entity.HasKey(x => x.Id);
-            });
-        }
-    }
+    //    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //    {
+    //        modelBuilder.Entity<OrderRequest>(entity =>
+    //        {
+    //            entity.HasKey(x => x.Id);
+    //        });
+    //    }
+    //}
     public class MockReportResponse
     {
         public int Counter { get; set; }
         public int Errors { get; set; }
-        public IEnumerable<OrderRequest> Items { get; set; }
+        public IEnumerable<Order> Items { get; set; }
     }
-    public class OrderRequest : Order
-    {
-        public int Id { get; set; }
-    }
+    //public class OrderRequest : Order
+    //{
+    //    public int Id { get; set; }
+    //}
     public class Order
     {
         public int OrderId { get; set; }
